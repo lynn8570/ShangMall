e61a3bb5ce7ca49e5bc9a08ab8ab626bc310c7f1
lynn8570 <lynn8570@gmail.com>
2018-06-26 15:02:42 +0800

    init

---
+++ app/src/main/java/lynn/mall/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package lynn.mall.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import lynn.mall.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
