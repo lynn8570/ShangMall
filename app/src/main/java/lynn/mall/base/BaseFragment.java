2bbb63d263cba671747558d2e57cd38a5fc4a0be
lynn8570 <lynn8570@gmail.com>
2018-06-27 09:24:43 +0800

    add fragment

--- app/src/main/java/lynn/mall/base/BaseFragment.java
+++ app/src/main/java/lynn/mall/base/BaseFragment.java
@@ -1,8 +1,49 @@
 package lynn.mall.base;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by zowee-laisc on 2018/6/26.
  */
 
-public class BaseFragment {
+public abstract class BaseFragment extends Fragment {
+    protected Context mContext;
+
+    /**
+     * 当该类被系统创建的时候回调
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = getActivity();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return initView();
+    }
+
+    //抽象类，由孩子实现，实现不同的效果
+    public abstract View initView();
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initData();
+    }
+
+    /**
+     * 当子类需要联网请求数据的时候，可以重写该方法，该方法中联网请求
+     */
+    public void initData() {
+    }
 }
---
+++ app/src/main/java/lynn/mall/fragment/CommunityFragment.java
@@ -0,0 +1,34 @@
+package lynn.mall.fragment;
+
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+import lynn.mall.base.BaseFragment;
+
+/**
+ * Created by zowee-laisc on 2018/6/26.
+ */
+
+public class CommunityFragment extends BaseFragment {
+    private final static String TAG = CommunityFragment.class.getSimpleName();
+
+    private TextView textView;
+
+    @Override
+    public View initView() {
+        textView = new TextView(mContext);
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextSize(25);
+        Log.e(TAG, "主页面的Fragment的UI被初始化了");
+        return textView;
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+        textView.setText("首页");
+        Log.e(TAG, "主页面的Fragment的数据被初始化了");
+    }
+}
