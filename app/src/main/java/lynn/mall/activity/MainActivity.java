b7b03ac2b6f73e99c8ce56cc67cacbb9910eef93
lynn8570 <lynn8570@gmail.com>
2018-06-27 10:36:49 +0800

    init homefragment in initial

--- app/src/main/java/lynn/mall/activity/MainActivity.java
+++ app/src/main/java/lynn/mall/activity/MainActivity.java
@@ -85,6 +85,10 @@ public class MainActivity extends FragmentActivity {
                 switchFragment(tempFragment, baseFragment);
             }
         });
+
+        BaseFragment baseFragment = getFragment(0);
+        switchFragment(tempFragment, baseFragment);
+
     }
 
     /**
